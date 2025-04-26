c48844e0e1ba816e1e6f50d44f889416dee3ff07
pckerneis <pc.kerneis@gmail.com>
2025-04-26 15:37:56 +0200

    Add markdown rendering

--- packages/spequoia-web/src/components/document-root/document-root.component.ts
+++ packages/spequoia-web/src/components/document-root/document-root.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {ParsedDocument} from 'spequoia-core/dist/model/parsed-document.model';
+import {MarkdownPipe} from '../../pipes/markdown.pipe';
 
 @Component({
   selector: 'app-document-root',
-  imports: [],
+  imports: [
+    MarkdownPipe
+  ],
   templateUrl: './document-root.component.html',
   styleUrl: './document-root.component.scss'
 })
---
+++ packages/spequoia-web/src/pipes/markdown.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as commonmark from 'commonmark';
+
+@Pipe({
+  name: 'markdown'
+})
+export class MarkdownPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    console.log('>>> value', value)
+    if (typeof value !== 'string') {
+      return value;
+    }
+
+    const reader = new commonmark.Parser();
+    const writer = new commonmark.HtmlRenderer();
+    const parsed = reader.parse(value);
+    return writer.render(parsed);
+  }
+}
