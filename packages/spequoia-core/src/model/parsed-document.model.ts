cbed2ab49418bfe900e0ea2fccb95a974cecea1f
pckerneis <pc.kerneis@gmail.com>
2025-04-26 12:23:56 +0200

    Generate JSON schema

---
+++ packages/spequoia-core/src/model/parsed-document.model.ts
@@ -0,0 +1,28 @@
+export interface ParsedDocument {
+  features: ParsedFeature[];
+}
+
+export interface ParsedFeature {
+  id: string;
+  description: string;
+  examples?: ParsedExample[];
+}
+
+export interface ParsedExample {
+  id: string;
+  description: string;
+  steps: ParsedStep[];
+}
+
+export interface ParsedStep {
+  operation: string;
+  params: Record<string, any>;
+  fragments: ParsedStepFragment[];
+}
+
+export type ParsedStepFragmentType = 'text' | 'variable';
+
+export interface ParsedStepFragment {
+  type: ParsedStepFragmentType;
+  value: string;
+}
---
+++ packages/spequoia-model/src/model/spequoia.model.ts
@@ -0,0 +1,15 @@
+export interface SpequoiaDocument {
+  features: SpequoiaFeature[];
+}
+
+export interface SpequoiaFeature {
+  id: string;
+  description: string;
+  examples?: SpequoiaExample[];
+}
+
+export interface SpequoiaExample {
+  id: string;
+  description: string;
+  steps?: Array<string | Record<string, any>>;
+}
