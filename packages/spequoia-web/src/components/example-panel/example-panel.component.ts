aebc8253604414f53addca956ae84ea0a05ebf81
pckerneis <pc.kerneis@gmail.com>
2025-05-05 20:57:49 +0200

    Add link to test results page

--- packages/spequoia-web/src/components/example-panel/example-panel.component.ts
+++ packages/spequoia-web/src/components/example-panel/example-panel.component.ts
@@ -1,17 +1,18 @@
-import { Component, computed, Input } from '@angular/core';
+import {Component, computed, Input, OnInit, signal} from '@angular/core';
 import { MarkdownPipe } from '../../pipes/markdown.pipe';
 import { ParsedExample } from 'spequoia-core/dist/model/parsed-document.model';
 import { WireframePlayerComponent } from '../wireframe-player/wireframe-player.component';
 import { DocumentService } from '../../services/document.service';
 import { RouterLink } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-example-panel',
-  imports: [MarkdownPipe, WireframePlayerComponent, RouterLink],
+  imports: [MarkdownPipe, RouterLink],
   templateUrl: './example-panel.component.html',
   styleUrl: './example-panel.component.scss',
 })
-export class ExamplePanelComponent {
+export class ExamplePanelComponent implements OnInit {
   @Input()
   example: ParsedExample | undefined;
 
@@ -23,5 +24,15 @@ export class ExamplePanelComponent {
     return this.documentService.document()?.defaultExecutor != null;
   });
 
-  constructor(private readonly documentService: DocumentService) {}
+  hasTestResults = signal(false);
+
+  constructor(private readonly documentService: DocumentService, private readonly http: HttpClient) {}
+
+  ngOnInit(): void {
+    if (this.example) {
+      this.http.get(`player-data/${this.example.id}/screenshot-manifest.json`).subscribe(manifest => {
+        this.hasTestResults.set(manifest != null);
+      });
+    }
+  }
 }
